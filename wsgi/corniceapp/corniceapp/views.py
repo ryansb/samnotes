6a84a2a58f3d2ff9e22bbd0fd522b3b01382cb4d
Ryan Brown <ryansb@csh.rit.edu>
2013-04-22 19:24:44 -0400

    Add samnotes service

--- wsgi/corniceapp/corniceapp/views.py
+++ wsgi/corniceapp/corniceapp/views.py
@@ -10,3 +10,16 @@ hello = Service(name='hello', path='/', description="Simplest app")
 def get_info(request):
     """Returns Hello in JSON."""
     return {'Hello': 'World'}
+
+samnotes = Service(name='samnotes', path='/sam/notes', description="stuff sam should remember")
+
+@samnotes.get()
+def get_info(request):
+    """Returns Hello in JSON."""
+    return {
+        "programs": [
+            "tmux",
+            "nload",
+            "iftop"
+        ]
+    }
